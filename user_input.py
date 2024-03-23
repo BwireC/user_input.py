e22dbc6bbb1dc352729e110d46bf5c3ab32ad26d
BwireC <159944235+BwireC@users.noreply.github.com>
2024-03-23 22:25:27 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,9 @@
+# Ask user for their name, age, and location.
+# Ask the user to enter their name and store it in the variable called "name"
+name=input('Enter your name: ')
+#Prompt the user to enter their age and store it in the variable called "age".
+age=input('Enter your age: ')
+# Prompt the user to enter their location and store it in the variable called "location".
+location=input('Enter your live location: ')
+# Now the message is printed here.
+print( 'Hello '+ name + ',you are '+ age+' years old and live in '+location)
